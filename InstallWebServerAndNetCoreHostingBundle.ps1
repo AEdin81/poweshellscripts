ec7cdf988c7f89213801491304300e16e8d46186
AEdin81 <51718429+AEdin81@users.noreply.github.com>
2022-01-02 21:35:51 +0100

    Update InstallWebServerAndNetCoreHostingBundle.ps1

--- InstallWebServerAndNetCoreHostingBundle.ps1
+++ InstallWebServerAndNetCoreHostingBundle.ps1
@@ -1,11 +1,1 @@
 powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
-
-$ProcName = "dotnet-hosting-3.1.22-win.exe"
-$WebFile = "https://download.visualstudio.microsoft.com/download/pr/5b681079-0068-4c70-be77-af30f1154a83/cd5d074d8328fbc0b3bebf87c88ae082/$ProcName"
- 
-Clear-Host
-
-(New-Object System.Net.WebClient).DownloadFile($WebFile,"$env:APPDATA\$ProcName")
-Start-Process ("$env:APPDATA\$ProcName")
-
-Start-Process -Wait -ArgumentList "/silent" -PassThru -FilePath "$env:APPDATA\$ProcName"
