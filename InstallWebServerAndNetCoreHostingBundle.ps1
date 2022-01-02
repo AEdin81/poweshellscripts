e6616410aa5bed990469e9c6b8c5c2dceb3f9431
AEdin81 <51718429+AEdin81@users.noreply.github.com>
2022-01-02 20:37:21 +0100

    Update InstallWebServerAndNetCoreHostingBundle.ps1

--- InstallWebServerAndNetCoreHostingBundle.ps1
+++ InstallWebServerAndNetCoreHostingBundle.ps1
@@ -1,3 +1,8 @@
 powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
 
-powershell -command PowerShell -ExecutionPolicy bypass -noprofile -windowstyle hidden -command (New-Object System.Net.WebClient).DownloadFile('https://download.visualstudio.microsoft.com/download/pr/5b681079-0068-4c70-be77-af30f1154a83/cd5d074d8328fbc0b3bebf87c88ae082/dotnet-hosting-3.1.22-win.exe',"$env:APPDATA\$ProcName");Start-Process ("$env:APPDATA\dotnet-hosting-3.1.22-win.exe")
+$ProcName = "dotnet-hosting-3.1.22-win.exe"
+$WebFile = "https://download.visualstudio.microsoft.com/download/pr/5b681079-0068-4c70-be77-af30f1154a83/cd5d074d8328fbc0b3bebf87c88ae082/$ProcName"
+ 
+Clear-Host
+
+Start-Process -Wait -ArgumentList "/silent" -PassThru -FilePath "$env:APPDATA\$ProcName"
